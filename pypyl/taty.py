70d9a00de95463e9272f9c2663fca0f40030f56d
Cabernetik2101 <tanya-tihonkova@mail.ru>
2024-05-18 06:05:15 +0300

    added new info

--- pypyl/taty.py
+++ pypyl/taty.py
@@ -2,3 +2,7 @@ print("Hello Tanya")
 name = int(input("Write your first name: "))
 name2 = int(input("Write your last name: "))
 print(name, name2)
+
+
+age = int(input("Write you age: "))
+print(age)
